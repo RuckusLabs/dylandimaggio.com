d3e82261a17596db32beb6e34ef44da47be73155
Damian Makki <damian@makki.pro>
2024-12-10 12:59:18 -0600

    add writing content

---
+++ src/components/Writings.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+
+export default function Writings() {
+
+  const [writings, setWritings] = useState([]);
+  const [data, setData] = useState([]);
+  const [writingId, setWritingId] = useState();
+
+  let { id } = useParams();
+
+  useEffect(() => {
+    fetch('/writing-database.json')
+      .then((response) => response.json())
+      .then((data) => {
+
+        console.log(data);
+        setWritings(data);
+        const matchedData = data.find((profile) => profile.id === Number(id));
+
+        setWritingId(setWritingId(matchedData.link));
+        setData(matchedData);
+
+      })
+      .catch((error) => console.error('Error loading profiles:', error));
+  }, [id]);
+
+  return (
+    <>
+      <div className="writing-content-wrapper">
+        {writings
+          .map((item) => (
+            <li key={item.id}>
+              <p><span className="publish-date">{item.publishDate}</span></p>
+              <h4>{item.title}</h4>
+              {item?.content?.split('\n').map((paragraph, index) => (
+                <p key={index}>{paragraph}</p>
+              ))}
+            </li>
+          ))}
+      </div>
+    </>
+  )
+}
