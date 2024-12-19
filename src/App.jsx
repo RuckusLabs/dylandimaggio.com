582507168b9725fbcccddbecb16ec35fde75b5dc
Damian Makki <damian@makki.pro>
2024-12-19 08:42:44 -0600

    update pathing

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <>
-      <Router basename="/dylandimaggio.com">
+      <Router>
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
