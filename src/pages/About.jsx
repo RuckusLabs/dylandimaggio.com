12c466cbd3902c0e29b4e1db47539c0e99660dc2
MrPyckle1 <dyl.dimaggio@gmail.com>
2025-05-03 09:39:27 -0700

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ function About() {
         <div className="intro">
           <h3>About</h3>
         </div>
-        <p>Hi, I’m Dylan—a screenwriter and storyteller with a background in film, fiction, and narrative design. I’ve written over a dozen original screenplays across sci-fi, horror, drama, and thrillers, including *Les Nettoyeurs* (2022) and *Santa Muerte* (sold). My work has been recognized by the Austin Film Festival and has helped bring emotionally grounded genre stories to life on screen.</p>
+        <p>poopopopopopopopopopopo Hi, I’m Dylan—a screenwriter and storyteller with a background in film, fiction, and narrative design. I’ve written over a dozen original screenplays across sci-fi, horror, drama, and thrillers, including *Les Nettoyeurs* (2022) and *Santa Muerte* (sold). My work has been recognized by the Austin Film Festival and has helped bring emotionally grounded genre stories to life on screen.</p>
         <p>Whether I’m writing a feature film, building a world for a game, or ghostwriting fiction, I approach every project with the same focus: character-first storytelling that hits hard. I’m drawn to stories about survival, grief, strange connections, and what it means to be human in a broken world.</p>
         <p>If you’re looking for a writer who can create immersive narratives, sharp dialogue, and clean structure—I’d love to connect and see what we can build together.</p>
       </div>
